13f476f57f8deb34a64f5a2a836c7434a9c20de7
Craig Jackson <cjackson@auth.netactuate.net>
2025-05-19 16:45:36 -0400

    version bumps

--- gona/client.go
+++ gona/client.go
@@ -17,7 +17,7 @@ import (
 
 // Version, BaseEndpoint, ContentType constants
 const (
-	Version      = "0.2.1"
+	Version      = "0.2.2"
 	BaseEndpoint = "https://vapi2.netactuate.com/api/"
 	ContentType  = "application/json"
 )
