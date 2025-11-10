15b3a1f04f196b930c2b30657c8454a283e28970
bill fumerola <bill@andyl.com>
2025-11-10 10:18:57 -0800

    chore(deps): bump version

--- gona/client.go
+++ gona/client.go
@@ -18,7 +18,7 @@ import (
 
 // Version, BaseEndpoint, ContentType constants
 const (
-	Version      = "0.3.0-pre1"
+	Version      = "0.3.0-pre2"
 	BaseEndpoint = "https://vapi2.netactuate.com/api/"
 	ContentType  = "application/json"
 )
